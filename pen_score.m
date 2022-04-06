a46011961f57bc424e755b06ec46db37b986beb9
HengyeChen <hengyechen@gmail.com>
2022-04-06 16:32:21 -0400

    Update pen_score.m

--- pen_score.m
+++ pen_score.m
@@ -1,4 +1,5 @@
 function penalty = pen_score(distance,wfactor,linker)
+%% this function calculate the penalty score from short linker
 
 if distance > linker% when distance is longer than 20bp, no penalty
     penalty = 0;
